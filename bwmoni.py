44e6c22aeaaabbcdee85fbdbd5f0d0e8a6d8e68e
Cameron Kilgore <ghostfreeman@gmail.com>
2015-12-18 18:06:30 -0500

    The value pulled for daily usage was download only, this sets it to both upload/download

--- bwmoni.py
+++ bwmoni.py
@@ -6,8 +6,8 @@ import sys
 total = len(sys.argv)
 
 if total == 3:
-  response = requests.get('http://routerlogin.net/traffic_stattbl.htm', auth=(sys.argv[1], sys.argv[2]));
-  soup = bs4.BeautifulSoup(response.text, "html.parser");
+  response = requests.get('http://routerlogin.net/traffic_stattbl.htm', auth=(sys.argv[1], sys.argv[2]))
+  soup = bs4.BeautifulSoup(response.text, "html.parser")
 
   # Pull out the data showing data used this month
   bandwidth = soup.select("td[width=85]")[1]
@@ -18,7 +18,7 @@ if total == 3:
   response2 = requests.get("http://routerlogin.net/traffic_meter.htm", auth=(sys.argv[1], sys.argv[2]))
   soup2 = bs4.BeautifulSoup(response2.text, "html.parser")
   #import pdb; pdb.set_trace()
-  bwToday = soup2.select("td[width=20%] p")[1]
+  bwToday = soup2.select("td[width=17%] p")[3]
   bwTodayCont = bwToday.contents[0]
 
   print "Total amount of bandwidth used this month: "+str(bwGig)+" GB ("+str(bwMeg)+" MBytes)"
