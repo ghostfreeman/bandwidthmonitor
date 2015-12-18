b0663096df6f0bc09f3c92b6d8d18df08b3971cd
Cameron Kilgore <ghostfreeman@users.noreply.github.com>
2015-12-18 15:21:19 -0500

    Update bwmoni.py
    Removed debug statement

--- bwmoni.py
+++ bwmoni.py
@@ -17,7 +17,6 @@ if total == 3:
 
   response2 = requests.get("http://routerlogin.net/traffic_meter.htm", auth=(sys.argv[1], sys.argv[2]))
   soup2 = bs4.BeautifulSoup(response2.text, "html.parser")
-  #import pdb; pdb.set_trace()
   bwToday = soup2.select("td[width=20%] p")[1]
   bwTodayCont = bwToday.contents[0]
 
