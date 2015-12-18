5601bde715fe4a6329a874b4981a2980c2fbce0e
Cameron Kilgore <ghostfreeman@gmail.com>
2015-12-18 15:20:16 -0500

    Added tracking of daily b/w usage

--- bwmoni.py
+++ bwmoni.py
@@ -13,7 +13,15 @@ if total == 3:
   bandwidth = soup.select("td[width=85]")[1]
   bwstr = bandwidth.contents[0]
   bwMeg = int(bwstr.split("/")[0])
-  bwGig = round((bwMeg / 1024), 1)
+  bwGig = round((bwMeg / 1024), 2)
+
+  response2 = requests.get("http://routerlogin.net/traffic_meter.htm", auth=(sys.argv[1], sys.argv[2]))
+  soup2 = bs4.BeautifulSoup(response2.text, "html.parser")
+  #import pdb; pdb.set_trace()
+  bwToday = soup2.select("td[width=20%] p")[1]
+  bwTodayCont = bwToday.contents[0]
+
   print "Total amount of bandwidth used this month: "+str(bwGig)+" GB ("+str(bwMeg)+" MBytes)"
+  print "Total used today: "+str(bwTodayCont)+" MBytes"
 else:
   print "Insufficient arguments. See README.md"
